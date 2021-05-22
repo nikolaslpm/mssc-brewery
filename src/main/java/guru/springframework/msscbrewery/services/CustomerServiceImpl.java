ee626613f8191e2d8c356b0e210ba130c94df527
Nikolas Manne <nikolas.manne@raisin.com>
2021-05-22 22:12:31 +0200

    Doing assingment

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -12,4 +12,18 @@ public class CustomerServiceImpl implements CustomerService {
     public CustomerDto getCustomerById(UUID customerId) {
         return CustomerDto.builder().id(customerId).name("Jirafales").build();
     }
+
+    @Override
+    public CustomerDto createCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder().id(UUID.randomUUID()).name("Raphael").build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+
+    }
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -2,11 +2,17 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.CustomerService;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -14,12 +20,32 @@ import java.util.UUID;
 @RequestMapping("/api/v1/customer")
 @RestController
 public class CustomerController {
+
     private final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {this.customerService = customerService;}
 
     @GetMapping("/{customerId}")
-    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable UUID customerId){
+    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable UUID customerId) {
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerDto> postCustomer(@RequestBody CustomerDto customerDto) {
+        CustomerDto savedCustomer = customerService.createCustomer(customerDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + savedCustomer.getId().toString());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@PathVariable UUID customerId, @RequestBody CustomerDto customerDto) {
+        customerService.updateCustomer(customerId, customerDto);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public void deleteCustomer(@PathVariable UUID customerId){
+        customerService.deleteCustomer(customerId);
     }
 }
