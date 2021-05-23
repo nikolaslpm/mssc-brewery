f6df056d68efc5eefa4f58bd1b346b1ea1712dda
Nikolas Manne <nikolas.manne@raisin.com>
2021-05-24 01:09:31 +0200

    Performed assignment, implemented tests and fixed exceptionHandler missing exception

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -5,8 +5,25 @@ import guru.springframework.msscbrewery.web.model.CustomerDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -15,6 +32,7 @@ import java.util.UUID;
 
 @RequestMapping("api/v1/customer")
 @RestController
+@Validated
 public class CustomerController {
 
     private CustomerService customerService;
@@ -24,13 +42,13 @@ public class CustomerController {
     }
 
     @GetMapping("/{customerId}")
-    public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId")  UUID customerId){
+    public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId") UUID customerId) {
 
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity handlePost(CustomerDto customerDto){
+    public ResponseEntity handlePost(@Valid @RequestBody @NotNull CustomerDto customerDto) {
         CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
 
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -41,12 +59,37 @@ public class CustomerController {
 
     @PutMapping("/{customerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void handleUpdate(@PathVariable("customerId") UUID customerId, CustomerDto customerDto){
+    public void handleUpdate(@PathVariable("customerId") UUID customerId, @Valid @RequestBody CustomerDto customerDto) {
         customerService.updateCustomer(customerId, customerDto);
     }
 
     @DeleteMapping("/{customerId}")
-    public void deleteById(@PathVariable("customerId")  UUID customerId){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable("customerId") UUID customerId) {
         customerService.deleteById(customerId);
+    }
+    //According to https://github.com/zalando/problem-spring-web/issues/3
+    //Invoked only when using Java Beans validation on the controller methods and adding the @Validated on the class
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
+        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+
+        e.getConstraintViolations().forEach(constraintViolation -> {
+            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    //Invoked when @Valid is used on the controller methods and the object arguments are implementing Java Beans validations
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List> validationErrorHandler(MethodArgumentNotValidException ex) {
+        List<String> errors = new ArrayList<>();
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errors.add(fieldError.getField() + " : " + fieldError.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/test/java/guru/springframework/msscbrewery/web/controller/CustomerControllerTest.java
@@ -0,0 +1,140 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msscbrewery.services.CustomerService;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+
+    @MockBean
+    CustomerService customerService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    CustomerDto validCustomer;
+
+    private final String API_BASE_PATH = "/api/v1/customer/";
+
+    @Before
+    public void setUp() {
+        validCustomer = CustomerDto.builder().id(UUID.randomUUID())
+            .name("Customer1")
+            .build();
+    }
+
+    @Test
+    public void getCustomer() throws Exception {
+        given(customerService.getCustomerById(any(UUID.class))).willReturn(validCustomer);
+
+        mockMvc.perform(get(API_BASE_PATH + validCustomer.getId().toString()).accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$.id", is(validCustomer.getId().toString())))
+            .andExpect(jsonPath("$.name", is(validCustomer.getName())));
+    }
+
+    @Test
+    public void handlePost() throws Exception {
+        //given
+        CustomerDto customer = validCustomer;
+        CustomerDto savedDto = CustomerDto.builder().id(UUID.randomUUID()).name("New Customer").build();
+        String customerDtoJson = objectMapper.writeValueAsString(customer);
+
+        given(customerService.saveNewCustomer(any())).willReturn(savedDto);
+
+        mockMvc.perform(post(API_BASE_PATH)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(customerDtoJson))
+            .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void givenNullNameWhenPostThenThrowException() throws Exception {
+        //given
+        CustomerDto customer = validCustomer;
+        customer.setName(null);
+        String customerDtoJson = objectMapper.writeValueAsString(customer);
+
+        mockMvc.perform(post(API_BASE_PATH)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(customerDtoJson))
+            .andExpect(content().string("[\"name : must not be blank\"]"))
+            .andExpect(status().isBadRequest())
+            .andReturn();
+
+        verify(customerService, times(0)).saveNewCustomer(any());
+    }
+
+    @Test
+    public void givenInvalidNameSizeWhenPostThenThrowException() throws Exception {
+        //given
+        CustomerDto customer = validCustomer;
+        customer.setName("A");
+        String customerDtoJson = objectMapper.writeValueAsString(customer);
+
+        mockMvc.perform(post(API_BASE_PATH)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(customerDtoJson))
+            .andExpect(content().string("[\"name : size must be between 3 and 100\"]"))
+            .andExpect(status().isBadRequest())
+            .andReturn();
+
+        verify(customerService, times(0)).saveNewCustomer(any());
+    }
+
+    @Test
+    public void handleUpdate() throws Exception {
+        //given
+        CustomerDto customer = validCustomer;
+        customer.setId(null);
+        String customerDtoJson = objectMapper.writeValueAsString(customer);
+
+        //when
+        mockMvc.perform(put(API_BASE_PATH + UUID.randomUUID())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(customerDtoJson))
+            .andExpect(status().isNoContent());
+
+        then(customerService).should().updateCustomer(any(), any());
+    }
+
+    @Test
+    public void handleDelete() throws Exception {
+        mockMvc.perform(delete(API_BASE_PATH + UUID.randomUUID())
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+
+        then(customerService).should().deleteById(any());
+    }
+}
